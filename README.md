a48689ea756e0f9ed375d90274dc6be6b479b0cc
mainmax <mainmax@gmail.com>
2021-10-28 21:42:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# terraform-aws-sg
+## Terraform module to create AWS EC2 Security Group from a firewall-like ruleset policy
+
+This Terraform module deploys an EC2 Security Group into specified VPC with ingress/egress rules generated from a 'policy document' in plain text format.
+
+From this:
+```code
+    IN  TCP  80   Any_IPv4,Any_IPv6 - HTTP Inbound
+    IN  TCP  443  0.0.0.0/0,::/0    - HTTPS Inbound
+    IN  TCP  8005 {bastion_ip}/32   - Tomcat admin from Bastion
+    IN  PING      0.0.0.0/0,::/0    - PING from Internet
+    OUT TCP  3306 {sg_db}           - Outbound to MySql DB
+    OUT TCP  443  pl-02cd2c6b       - DynamoDB Prefix List
+```
+to this:
+![](https://github.com/mainmax/terraform-aws-sg/raw/master/aws-sg-rules.png)
+
+Motivation for this module was to allow people that are not familiar with terraform (like Network and InfoSec guys) to be able to create/review Security Groups configurations without HCL in a way. It also allows to directly copy/paste more readable Security Group rules between change tickets, technical documentation (if you maintain one) and TF templates. 
+
+## Usage in Terraform
+
+Simple rule using TERMs replacement from built-in vocabulary (check main.tf for available values, fork this repo to add your own)
+
+SSH -> TCP 22, Any_IPv4 -> 0.0.0.0/0
+
+```hcl
+module "sg_bastion" {
+  source      = "../../"
+  name        = "TF Test Bastion"
+  description = "Test SG for Bastion"
+  vpc_id      = aws_vpc.test.id
+  rules = "ingress SSH Any_IPv4 - SSH for Bastion"
+}
+```
+
+Rules can be formatted with spaces and TABs
+
+Dynamic values that are available during Apply can be added with combination of
+{var} template and additional module parameter "rules_vars", where "var" = value 
+
+```hcl
+module "sg_web" {
+  source      = "../../"
+  name        = "TF Test Web"
+  description = "Test SG for Web services"
+  vpc_id      = aws_vpc.test.id
+  rules = <<EOF
+    IN  TCP  80   Any_IPv4,Any_IPv6 - HTTP Inbound
+    IN  TCP  443  0.0.0.0/0,::/0    - HTTPS Inbound
+    IN  TCP  8005 {bastion_ip}/32   - Tomcat admin from Bastion
+    IN  PING      0.0.0.0/0,::/0    - PING from Internet
+    OUT TCP  3306 {sg_db}           - Outbound to MySql DB
+    OUT TCP  443  pl-02cd2c6b       - DynamoDB Prefix List
+  EOF
+  rules_vars = {
+    "sg_db"      = module.sg_db.id
+    "bastion_ip" = var.bastion_ip
+  }
+}
+```
+
+Lines can be commented with "#" in the beginning
+
+Rule description can be ommited 
+
+Port ranges are supported with dash and no spaces "from_port-to_port"
+
+```hcl
+module "sg_db" {
+  source      = "../../"
+  name        = "TF Test DB"
+  description = "Test SG for DB services"
+  vpc_id      = aws_vpc.test.id
+  rules = <<EOF
+  # Inbound Rules
+    IN TCP 3306 {sg_web} - MySql Inbound from WEB
+    IN ALL TRAFFIC self - Master-Slave replication
+    IN ICMP ALL {bastion_ip}/32 - ICMP from Bastion
+  # Outbound Rules
+    OUT ALL TRAFFIC self
+    OUT UDP 514-517 10.0.0.5/32,10.0.1.5/32 - SYSLOG Servers
+  EOF
+  rules_vars = {
+    "sg_web"     = module.sg_web.id
+    "bastion_ip" = var.bastion_ip
+  }
+}
+```
+
+There is some extra logic goes into creating Terraform State in such a way, that adding/deleting/changing a particular rule will not require "replacement" of other rules or the whole Security Group. It should help someone who is using Sentinel or OPA policies to validate deployment footprint when doing incremental changes to your infrastructure.
+
+```bash
+$ terraform state list
+...
+module.sg_db.aws_security_group.this
+module.sg_db.aws_security_group_rule.this["IN_ALL_TRAFFIC_self"]
+module.sg_db.aws_security_group_rule.this["IN_ICMP_ALL_bastion_ip/32"]
+module.sg_db.aws_security_group_rule.this["IN_TCP_3306_sg_web"]
+module.sg_db.aws_security_group_rule.this["OUT_ALL_TRAFFIC_self"]
+module.sg_db.aws_security_group_rule.this["OUT_UDP_514-517_10.0.0.5/32,10.0.1.5/32"]
+...
+```
+
+## Authors
+
+Originally created by [mainmax](http://github.com/mainmax)
+
+## License
+
+[MIT](LICENSE)
---
+++ main.tf
@@ -0,0 +1,90 @@
+resource  "aws_security_group" "this" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+}
+
+variable "terms" {
+  default = {}
+}
+
+locals {
+  map = merge( 
+    {
+      "IN"              = "ingress"
+      "OUT"             = "egress"
+      "ALL TRAFFIC"     = "ALL ALL"                                
+      "Any_IPv4"        = "0.0.0.0/0"
+      "Any_IPv6"        = "::/0"
+      "SSH"             = "TCP 22"
+      "SMTP"            = "TCP 25"
+      "DNS_UDP"         = "UDP 53"
+      "DNS_TCP"         = "TCP 53"
+      "HTTP"            = "TCP 80"
+      "POP3"            = "TCP 110"
+      "IMAP"            = "TCP 143"
+      "LDAP"            = "TCP 386"
+      "HTTPS"           = "TCP 443"
+      "SMB"             = "TCP 445"
+      "SMTPS"           = "TCP 465"
+      "IMAPS"           = "TCP 993"
+      "POP3S"           = "TCP 995"
+      "MSSQL"           = "TCP 1433"
+      "NFS"             = "TCP 2049"
+      "MYSQL"           = "TCP 3306"
+      "RDP"             = "TCP 3389"
+      "Redshift"        = "TCP 5439"
+      "PostgreSQL"      = "TCP 5432"
+      "Oracle-RDS"      = "TCP 1521"
+      "WinRM-HTTP"      = "TCP 5985"
+      "WinRM-HTTPS"     = "TCP 5986"
+      "Elastic Graphics"= "TCP 5432"
+      "PING"            = "ICMP 8-0"
+    }, 
+    var.terms,
+    {for k,v in var.rules_vars: "{${k}}" => v}
+  )
+
+  r  = replace(replace(var.rules,
+        "/(?m)^[ \\t]+|[ \\t]+$/",""),            # trim whitespaces
+        "/[ \\t]+/"," ")                          # convert multiple whitespaces into a single one
+  
+  rr = {for idx, line in split("\n",local.r):     # extract array of "rules"
+        idx => can(split(" - ", line)[0]) ? split(" - ", line)[0] : line
+        if (line != "" && substr(line,0,1) != "#")
+      }
+
+  rd = {for idx, line in split("\n",local.r):     # extract array of "descriptions"
+        idx => can(split(" - ", line)[1]) ? split(" - ", line)[1] : ""
+        if (line != "" && substr(line,0,1) != "#")
+      }
+
+  rk = {for idx, l in local.rr:                                 # create resource keys map
+        join("_",split(" ",replace(l,"/{(.*)}/","$1"))) => idx  # with values referencing rule "line number"
+        }                                                       # remove templating symbls {...} for cleaner resource keys
+  
+  rv = {for idx, line in local.rr:                                      # create rules values map
+        idx => split(" ", format(                                       # with keys referencing rule "line number"
+          replace(line, "/(${join("|", keys(local.map))})/", "%s"),     # some dictionary replacement magic   
+          [                                                             # thanks to https://stackoverflow.com/users/1239484/allejo
+            for value in flatten(regexall("(${join("|", keys(local.map))})", line)) :
+              lookup(local.map, value)
+          ]...
+        ))}
+}
+
+resource "aws_security_group_rule" "this" {
+  for_each = local.rk
+
+  type        = local.rv[each.value][0]
+  from_port   = local.rv[each.value][2] == "ALL" ? -1 : can(regex("-",local.rv[each.value][2])) ? split("-",local.rv[each.value][2])[0] : local.rv[each.value][2]
+  to_port     = local.rv[each.value][2] == "ALL" ? -1 : can(regex("-",local.rv[each.value][2])) ? split("-",local.rv[each.value][2])[1] : local.rv[each.value][2]
+  protocol    = local.rv[each.value][1]
+  cidr_blocks       = can(regex("[.]",local.rv[each.value][3])) ? [for c in split(",",local.rv[each.value][3]): c if can(regex("[.]", c))] : null
+  ipv6_cidr_blocks  = can(regex("[:]",local.rv[each.value][3])) ? [for c in split(",",local.rv[each.value][3]): c if can(regex("[:]", c))] : null
+  source_security_group_id = can(regex("^sg-", local.rv[each.value][3])) ? local.rv[each.value][3] : null
+  prefix_list_ids          = can(regex("^pl-", local.rv[each.value][3])) ? split(",", local.rv[each.value][3]) : null
+  self                     = local.rv[each.value][3] == "self" ? true : null
+  description = local.rd[each.value]
+  security_group_id = aws_security_group.this.id
+}
