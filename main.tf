c98490d3a73d9f91706c1de7b8e2dba7b9bcd7e1
Your Name <you@example.com>
2022-02-17 14:46:22 -0500

    Code cleanup

--- main.tf
+++ main.tf
@@ -5,11 +5,13 @@ resource  "aws_security_group" "this" {
 }
 
 locals {
-  map = merge( 
+  # Limit KEY length to 64 chars
+  terms = merge( 
     {
       "IN"              = "ingress"
       "OUT"             = "egress"
-      "ALL TRAFFIC"     = "ALL ALL"                                
+      "ALL"             = "-1"
+      "ALL TRAFFIC"     = "-1 -1"                                
       "Any_IPv4"        = "0.0.0.0/0"
       "Any_IPv6"        = "::/0"
       "SSH"             = "TCP 22"
@@ -40,18 +42,25 @@ locals {
     {for k,v in var.rules_vars: "{${k}}" => v}
   )
 
-  r  = replace(replace(var.rules,
-        "/(?m)^[ \\t]+|[ \\t]+$/",""),            # trim whitespaces
-        "/[ \\t]+/"," ")                          # convert multiple whitespaces into a single one
+  termsks = flatten([                                     # extract TERMs and sort them longest->shortest
+    for i in range(64,0): [                               # as regex will process them in order
+      for k in keys(local.terms): k if (length(k) == i)
+      ]
+    ])
+
+  termsregx = join("|", local.termsks)                    # prepare alteration regex for TERM replacement
+
+  r  = {for idx, line in split("\n", var.rules): idx =>
+        trimspace(replace(line, "/[ \\t]+/"," "))               # convert multiple whitespaces into a single one and trim
+        if (line != "" && substr(trimspace(line),0,1) != "#")   # skip empty and commented lines
+    }
   
-  rr = {for idx, line in split("\n",local.r):     # extract array of "rules"
-        idx => can(split(" - ", line)[0]) ? split(" - ", line)[0] : line
-        if (line != "" && substr(line,0,1) != "#")
+  rr = {for idx, line in local.r:                               # extract array of "rules"
+        idx => try(split(" - ", line)[0], line)
       }
 
-  rd = {for idx, line in split("\n",local.r):     # extract array of "descriptions"
-        idx => can(split(" - ", line)[1]) ? split(" - ", line)[1] : ""
-        if (line != "" && substr(line,0,1) != "#")
+  rd = {for idx, line in local.r:                               # extract array of "descriptions"
+        idx => try(split(" - ", line)[1], "")
       }
 
   rk = {for idx, l in local.rr:                                 # create resource keys map
@@ -60,26 +69,42 @@ locals {
   
   rv = {for idx, line in local.rr:                                      # create rules values map
         idx => split(" ", format(                                       # with keys referencing rule "line number"
-          replace(line, "/(${join("|", keys(local.map))})/", "%s"),     # some dictionary replacement magic   
+          replace(line, "/(${local.termsregx})/", "%s"),                # some dictionary replacement magic   
           [                                                             # thanks to https://stackoverflow.com/users/1239484/allejo
-            for value in flatten(regexall("(${join("|", keys(local.map))})", line)) :
-              lookup(local.map, value)
+            for value in flatten(regexall("(${local.termsregx})", line)) :
+              lookup(local.terms, value)
           ]...
         ))}
+
+  rvm = {for idx, v in local.rv:
+           idx => {
+             type         = v[0]
+             protocol     = v[1]
+             from_port    = try(regex("(\\d+)-(\\d+)",v[2])[0], v[2])
+             to_port      = try(regex("(\\d+)-(\\d+)",v[2])[1], v[2])
+             cidr_blocks      = can(regex("[.]",v[3])) ? [for c in split(",",v[3]): c if can(regex("[.]", c))] : null
+             ipv6_cidr_blocks = can(regex("[:]",v[3])) ? [for c in split(",",v[3]): c if can(regex("[:]", c))] : null
+             source_security_group_id = try(regex("^(sg-.*)", v[3])[0], null)
+             prefix_list_ids          = try(flatten(regexall("(pl-\\w*)", v[3])), null)
+             self                     = try({"self": true}[v[3]], null)
+             description  = local.rd[idx]
+           }
+
+  }
 }
 
 resource "aws_security_group_rule" "this" {
   for_each = local.rk
-
-  type        = local.rv[each.value][0]
-  from_port   = local.rv[each.value][2] == "ALL" ? -1 : can(regex("-",local.rv[each.value][2])) ? split("-",local.rv[each.value][2])[0] : local.rv[each.value][2]
-  to_port     = local.rv[each.value][2] == "ALL" ? -1 : can(regex("-",local.rv[each.value][2])) ? split("-",local.rv[each.value][2])[1] : local.rv[each.value][2]
-  protocol    = local.rv[each.value][1]
-  cidr_blocks       = can(regex("[.]",local.rv[each.value][3])) ? [for c in split(",",local.rv[each.value][3]): c if can(regex("[.]", c))] : null
-  ipv6_cidr_blocks  = can(regex("[:]",local.rv[each.value][3])) ? [for c in split(",",local.rv[each.value][3]): c if can(regex("[:]", c))] : null
-  source_security_group_id = can(regex("^sg-", local.rv[each.value][3])) ? local.rv[each.value][3] : null
-  prefix_list_ids          = can(regex("^pl-", local.rv[each.value][3])) ? split(",", local.rv[each.value][3]) : null
-  self                     = local.rv[each.value][3] == "self" ? true : null
-  description = local.rd[each.value]
   security_group_id = aws_security_group.this.id
+
+  type                     = local.rvm[each.value].type
+  protocol                 = local.rvm[each.value].protocol
+  from_port                = local.rvm[each.value].from_port
+  to_port                  = local.rvm[each.value].to_port
+  cidr_blocks              = local.rvm[each.value].cidr_blocks
+  ipv6_cidr_blocks         = local.rvm[each.value].ipv6_cidr_blocks
+  source_security_group_id = local.rvm[each.value].source_security_group_id
+  prefix_list_ids          = local.rvm[each.value].prefix_list_ids
+  self                     = local.rvm[each.value].self
+  description              = local.rvm[each.value].description
 }
